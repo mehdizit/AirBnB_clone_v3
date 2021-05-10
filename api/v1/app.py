987b5a5950ff580fa5bcb0f5534c10854dd37295
mehdizit <zitounimehdi.it@gmail.com>
2021-05-10 22:49:47 +0100

    up

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,12 +5,14 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
